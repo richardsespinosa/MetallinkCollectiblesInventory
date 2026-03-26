a2ae724a5909fa385df63a69319133faea95fc2e
Richard Espinosa <rsespinosa@aol.com>
2026-03-26 15:54:29 -0700

    Adding Data to ComicBooks, FunkoPops and SkateDeck Tables

---
+++ GitHubSQL4.sql
@@ -0,0 +1,37 @@
+
+INSERT INTO ComicBooks (GradingInfo, Grade, Publisher, Title, IssueNumber, IssueDate, Artist, Newstand, HeaderNotes, CertNumber, SignedBy, ComicBookNotes)
+VALUES 
+      ('CGC Signature Series'
+      ,'9.8' 
+      ,'Image'
+      ,'Spawn'
+      ,2
+      ,'06/92'
+      ,'Todd McFarlane'
+      ,1
+      ,'Newstand Edition'
+      ,4668392001
+      ,'Todd McFarlane'
+      ,'1st Appearance of the Violator. 3rd image "I" logo. Spawn pin-up Rick Leonardi');
+
+INSERT INTO FunkoPops (Name, CharacterName, Number, ConventionEdition)
+VALUES
+      ('Conan'
+      ,'Superman Conan'
+      ,05
+      ,'SDCC 2016 Limited Edition');
+
+INSERT INTO SkateDeck (Name, ConventionEdition, PriceBought, Notes)
+VALUES
+      ('King of the Hill'
+      ,'SDCC 2025'
+      ,'125.00'
+      ,'Limited Edition to 125');
+
+Select * from ComicBooks
+Select * from FunkoPops
+Select * from SkateDeck
+
+
+
+
