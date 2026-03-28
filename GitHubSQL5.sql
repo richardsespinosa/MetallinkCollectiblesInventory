cbfd1415f0c4cf3ce189b4c53d44a1cace599167
Richard Espinosa <rsespinosa@aol.com>
2026-03-28 14:28:45 -0700

    Updating a Table value

---
+++ GitHubSQL5.sql
@@ -0,0 +1,8 @@
+
+UPDATE ComicBooks
+SET Newstand = 0
+WHERE ID IN (114, 115, 116, 117, 121, 122, 123, 124, 125, 126, 137, 138, 139);
+
+
+Select * From ComicBooks WHERE ID IN (114, 115, 116, 117, 121, 122, 123, 124, 125, 126, 137, 138, 139)
+      
